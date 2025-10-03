1b9a9f9778459922640e7a1a0566cf45b06b9f18
mehedi <mahbub@evidentbd.com>
2025-10-03 21:34:37 +0600

    change database credentials for production

--- database/database.go
+++ database/database.go
@@ -18,13 +18,14 @@ func ConnectDB() (*gorm.DB, error) {
 		log.Panic("Error loading .Env File")
 	}
 
-	dbname := os.Getenv("DB_NAME")
-	dbuser := os.Getenv("DB_USER")
-	dbport := os.Getenv("DB_PORT")
-	dbhost := os.Getenv("DB_HOST")
-	dbpass := os.Getenv("DB_PASS")
+	// dbname := os.Getenv("DB_NAME")
+	// dbuser := os.Getenv("DB_USER")
+	// dbport := os.Getenv("DB_PORT")
+	// dbhost := os.Getenv("DB_HOST")
+	// dbpass := os.Getenv("DB_PASS")
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpass, dbname)
+	// dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpass, dbname)
+	dsn:=os.Getenv("DATABASE_URL")
 
 	if dsn == "" {
 		log.Panic("Error Loading .Env File")
